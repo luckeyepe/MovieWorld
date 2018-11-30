15857a82fef45a566f3919b5201bf74a586fcac8
luckeyepe <Gorilaman2>
2018-11-30 14:42:39 +0800

    added the buy ticket activity (branch selection and seat selection)

---
+++ app/src/main/java/com/example/morkince/movieworld/activities/BuyTicketBranchActivity.kt
@@ -0,0 +1,13 @@
+package com.example.morkince.movieworld.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.morkince.movieworld.R
+
+class BuyTicketBranchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buy_ticket_branch)
+    }
+}
